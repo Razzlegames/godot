6cfe3b883d55ce7b3f3f96c16f692ffa5b8e9296
Kyle Luce <razzlegames@gmail.com>
2016-03-12 10:08:01 -0700

    Easy build script for what I need

---
+++ build_godot_kyle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir platform/android/java/libs
+mkdir platform/android/java/libs/armeabi
+nice -n 19 scons platform=x11 && \
+nice -n 19 scons platform=android target=release_debug && \
+nice -n 19 scons platform=android target=release && \
+ln -fs $PWD/bin/libgodot.android.opt.armv7.neon.so platform/android/java/libs/armeabi/libgodot_android.so && \
+cd platform/android/java && ./gradlew build --stacktrace && cd ../../../
+
